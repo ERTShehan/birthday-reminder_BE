722a245342bf037953a28daddb8d3844e5db9c3f
Shehan <ertsshehan@gmail.com>
2026-04-26 08:53:04 +0530

    add birthday routes and cron job initialization in index file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db';
+import birthdayRoutes from './routes/birthdayRoutes';
+import { startCronJob } from './jobs/cronJob';
 
 dotenv.config();
 
@@ -12,10 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/birthdays', birthdayRoutes);
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+startCronJob();
 
 const PORT = process.env.PORT || 5000;
 
