56a115442fd76dc3ea7f15cfd7698bcad2cdac8a
Shehan <ertsshehan@gmail.com>
2026-04-27 08:19:25 +0530

    update cron job schedule to run at 8:30 AM daily

--- src/jobs/cronJob.ts
+++ src/jobs/cronJob.ts
@@ -3,7 +3,7 @@ import Birthday from '../models/Birthday';
 import { sendBirthdayReminderEmail } from '../services/emailService';
 
 export const startCronJob = () => {
-  cron.schedule('0 8 * * *', async () => {
+  cron.schedule('30 8 * * *', async () => {
     console.log('Running daily birthday check...');
     try {
       const tomorrow = new Date();
